f884b3faed48bcf49911d262c19af23aac4da735
i_yunu3 <i_yunu3@gmail.com>
2023-06-03 11:46:07 +0330

    new changes in flask app:

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 # import necessary lib
-from flask import Flask, request, render_template, jsonify, g
+from flask import Flask, request, jsonify, g
 from datetime import datetime
-import sqlite3
+import mysql.connector
 
 # create web application
 app = Flask(__name__)
 
-DATABASE = 'entries.db'
+# create a connection to the MySQL database
+config = {
+    'user': 'your_username',
+    'password': 'your_password',
+    'host': 'your_host',
+    'database': 'your_database',
+}
+cnx = mysql.connector.connect(**config)
 
 def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
+    return cnx
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -22,9 +26,8 @@ def close_connection(exception):
 
 # initialize database
 with app.app_context():
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT, time TEXT)''' )
+    cursor = cnx.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY AUTO_INCREMENT, content TEXT, time DATE)''')
 
 # 'C'RUD: Create
 @app.route("/", methods=["POST", "GET"])
@@ -38,36 +41,34 @@ def home():
 
         with app.app_context():
             # interact with database to insert data into it
-            db = get_db()
-            cursor = db.cursor()
-            cursor.execute("INSERT INTO entries (content, time) VALUES (?, ?)", (entry_content, time))
+            cursor = get_db().cursor()
+            cursor.execute("INSERT INTO entries (content, time) VALUES (%s, %s)", (entry_content, time))
 
             # commit the changes
-            db.commit()
-            
+            get_db().commit()
+
             return "Entry has been added!"
 
-    return "Invalid request"
+    return "Hello, World!"
 
 # C'R'UD: Read
 @app.route("/content")
 def show_content():
     with app.app_context():
         # execute the sql code to select all data has been stored
-        db = get_db()
-        cursor = db.cursor()
+        cursor = get_db().cursor()
         cursor.execute("SELECT * FROM entries")
         rows = cursor.fetchall()
 
         entries = {}
 
         for row in rows:
-            entries[row[0]] = {"content": row[1], "time": row[2]}
+            entries[row[0]] = {"content": row[1], "time": row[2].strftime("%Y-%m-%d")}
 
     return jsonify(entries)
 
 # CR'U'D: Update
-@app.route("/update", methods=["POST"])
+@app.route("/update", methods=["PUT"])
 def update_entry():
     # get the id from user
     entry_id = request.form.get("id")
@@ -77,22 +78,20 @@ def update_entry():
 
     with app.app_context():
         # execute sql code in oeder to update the content
-        db = get_db()
-        cursor = db.cursor()
-        cursor.execute("UPDATE entries SET content=? WHERE id=?", (new_content, entry_id))
-        db.commit()
+        cursor = get_db().cursor()
+        cursor.execute("UPDATE entries SET content=%s WHERE id=%s", (new_content, entry_id))
+        get_db().commit()
 
     return "Entry has been updated!"
 
-@app.route("/delete",methods=["POST"])
+@app.route("/delete",methods=["DELETE"])
 def del_entry():
     entry_id = request.form.get("id")
 
     with app.app_context():
-        db = get_db()
-        cursor = db.cursor()
-        cursor.execute("DELETE FROM entries WHERE id=?", (entry_id,))
-        db.commit()
+        cursor = get_db().cursor()
+        cursor.execute("DELETE FROM entries WHERE id=%s", (entry_id,))
+        get_db().commit()
 
     return "Entry has been deleted!"
 
